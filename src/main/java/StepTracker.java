6d0038ca3ea1661b0b6b4661f25e0c4bd69f07c2
LucaTieman2 <143014925+LucaTieman2@users.noreply.github.com>
2024-04-22 10:12:19 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,12 +1,20 @@
 import java.util.ArrayList;
 public class StepTracker
 {
-  private int days, 
-  public StepTracker () {
-  
+  private int aDays, nDays, aSteps, dSteps;
+  public StepTracker (int a) {
+    aDays = 0;
+    aSteps = dSteps / nDaysl;
+    
   }
   public int addDailySteps (int x) {
-    
+    if (x >= 10000)  {
+      nDays++;
+      aDays++;
+    } else {
+      nDays++;
+    }
+    return aDays;
   }
   public double averageSteps () {
   
